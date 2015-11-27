b866155b6b2d268a5d8182e25b6902afc8b46726
FuRa1 <lalique.surf@gmail.ru>
2015-11-27 18:26:07 +0200

    $ into H1 text # into H2 text

--- js/page.js
+++ js/page.js
@@ -3,5 +3,23 @@ $( document ).ready(function() {
         var data = $("textarea").val();
         console.log(data);
         $("#result").html(data);
+        var textArray = $("textarea").val().split(" ");
+        console.log(textArray);
+        var htmlArray =[];
+        for(var i=0; i<textArray.length; i++){
+            var indicateH1 = textArray[i].indexOf("$");
+             if(indicateH1>=0){
+                 htmlArray[i] = "<h1>"+textArray[i].slice(1,textArray[i].length+1)+"</h1>";
+             }else{
+                 var indicateH2 = textArray[i].indexOf("#");
+                    if(indicateH2>=0){
+                        htmlArray[i] = "<h2>"+textArray[i].slice(1,textArray[i].length+1)+"</h2>";
+                    }else{
+                        htmlArray[i]=textArray[i];
+                    }
+             }
+        }
+        $("#result").html(htmlArray.join(" "));
+        console.log(htmlArray);
     })
 });
