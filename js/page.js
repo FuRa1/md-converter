724e130b2b4e06900b01e11b0259b848b1c50c61
FuRa1 <lalique.surf@gmail.ru>
2015-12-06 22:59:56 +0200

    line 27 undefined

--- js/page.js
+++ js/page.js
@@ -1,25 +1,36 @@
-$( document ).ready(function() {
+$(function(){
+   /* var RegObj = {
+        block: /(#{1,6}.*)/,
+        header: /(#{1,6})(.*)/,
+        em: /\*{1}((.+?\n?)+?)\*{1}/g,
+        strong: /\*{2}((.+?\n?)+?)\*{2}/g,
+        link: /\[(.*)\]\((.*)\)/
+    }; */
+    var block = /(#{1,6}.*)/;
+    var header = /(#{1,6})(.*)/;
+    var em = /\*{1}((.+?\n?)+?)\*{1}/g;
+    var strong = /\*{2}((.+?\n?)+?)\*{2}/g;
+    var link = /\[(.*)\]\((.*)\)/;
+    var textArray=[];
+    var parsedTextArray =[];
+
     $("textarea").keyup(function() {
-        var data = $("textarea").val();
-        console.log(data);
-        $("#result").html(data);
-        var textArray = $("textarea").val().split(" ");
-        console.log(textArray);
-        var htmlArray =[];
-        for(var i=0; i<textArray.length; i++){
-            var indicateH1 = textArray[i].indexOf("$");
-             if(indicateH1>=0){
-                 htmlArray[i] = "<h1>"+textArray[i].slice(1,textArray[i].length+1)+"</h1>";
-             }else{
-                 var indicateH2 = textArray[i].indexOf("#");
-                    if(indicateH2>=0){
-                        htmlArray[i] = "<h2>"+textArray[i].slice(1,textArray[i].length+1)+"</h2>";
-                    }else{
-                        htmlArray[i]=textArray[i];
-                    }
-             }
-        }
-        $("#result").html(htmlArray.join(" "));
-        console.log(htmlArray);
-    })
-});
+        textArray = $("textarea").val().split(block);
+        Pars(textArray);
+    });
+
+function Pars(text){
+        var textClone = text;
+    alert(typeof (textClone));
+    for(var key in textClone){
+        var newText = textClone.key;
+        console.log(newText);
+        newText = newText.replace(strong, "<strong>"+"$1"+"</strong>");
+        newText = newText.replace(em, "<em>"+"$1"+"</em>");
+        newText = newText.replace(link,"<a href=\""+"$2"+"\">"+"$1"+"</a>");
+        newText = newText.replace(header,"<h"+"$1".length+">"+"$2"+"</h"+"$1".length+">");
+        parsedTextArray.push(newText);
+    }
+    $("#result").html(parsedTextArray.join(/\n/));
+};
+})
