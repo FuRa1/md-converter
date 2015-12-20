3ca8dd14b8adcd77a7f0b257ae0d959227d36007
FuRa1 <lalique.surf@gmail.ru>
2015-12-20 09:15:28 +0200

    total

--- js/page.js
+++ js/page.js
@@ -1,37 +1,8 @@
-$(function(){
-    var replacer={
-        strong: function(text){
-            return text.replace(regs.strong,strings.strong)},
-        em: function(text){
-            return text.replace(regs.em,strings.em)},
-        link: function(text){
-            return text.replace(regs.link,strings.link)},
-        para: function(text){
-            return text.replace(regs.para,strings.para)},
-        header: function(text){
-            return text.replace(regs.header,function($1,$2){
-                var head = $2.length;
-                var content = $1.slice(head,$1.length+1);
-                return "<h"+head+" class=\"headers\">"+"   "+content+"</h"+head+">"})}
-    };
-    var regs = {
-        block: /(#{1,6}.*)/,
-        header: /(#{1,6})(.*)/g,
-        strong: /\*{2}((.+?\n?)+?)\*{2}/g,
-        em: /\*{1}((.+?\n?)+?)\*{1}/g,
-        link: /\[(.*)\]\((.*)\)/,
-        para: /%{1}(.*)/g};
-    var strings ={
-        strong: "<strong>"+"$1"+"</strong>",
-        em: "<em>"+"$1"+"</em>",
-        link: "<a href=\""+"$2"+"\">"+"$1"+"</a>",
-        para: "<p"+" class=\"newP\">"+"   "+"$1"+"</p>"};
-        $("#convert").click(function() {
-            Pars($("#source").val());});
+$(function () {
+    $("#convert").click(function () {
+        var sourseText = $("#source").val();
+        var resultText = window.parse(sourseText);
 
-        function Pars(text){
-            for(var key in replacer ){
-                text=replacer[key](text)
-            }
-            $("#result").html(text); };
-})
+        $("#result").html(resultText);
+    });
+});
--- js/parser.js
+++ js/parser.js
@@ -1,18 +1,24 @@
-$(function(){
-    var replacer={
-        strong: function(text){
-            return text.replace(regs.strong,strings.strong)},
-        em: function(text){
-            return text.replace(regs.em,strings.em)},
-        link: function(text){
-            return text.replace(regs.link,strings.link)},
-        para: function(text){
-            return text.replace(regs.para,strings.para)},
-        header: function(text){
-            return text.replace(regs.header,function($1,$2){
+(function (window) {
+    var replacer = {
+        strong: function (text) {
+            return text.replace(regs.strong, strings.strong)
+        },
+        em: function (text) {
+            return text.replace(regs.em, strings.em)
+        },
+        link: function (text) {
+            return text.replace(regs.link, strings.link)
+        },
+        para: function (text) {
+            return text.replace(regs.para, strings.para)
+        },
+        header: function (text) {
+            return text.replace(regs.header, function ($1, $2) {
                 var head = $2.length;
-                var content = $1.slice(head,$1.length+1);
-                return "<h"+head+" class=\"headers\">"+"   "+content+"</h"+head+">"})}
+                var content = $1.slice(head, $1.length + 1);
+                return "<h" + head + " class=\"headers\">" + "   " + content + "</h" + head + ">"
+            })
+        }
     };
     var regs = {
         block: /(#{1,6}.*)/,
@@ -20,10 +26,21 @@ $(function(){
         strong: /\*{2}((.+?\n?)+?)\*{2}/g,
         em: /\*{1}((.+?\n?)+?)\*{1}/g,
         link: /\[(.*)\]\((.*)\)/,
-        para: /%{1}(.*)/g};
-    var strings ={
-        strong: "<strong>"+"$1"+"</strong>",
-        em: "<em>"+"$1"+"</em>",
-        link: "<a href=\""+"$2"+"\">"+"$1"+"</a>",
-        para: "<p"+" class=\"newP\">"+"   "+"$1"+"</p>"};
-})
+        para: /%{1}(.*)/g
+    };
+    var strings = {
+        strong: "<strong>" + "$1" + "</strong>",
+        em: "<em>" + "$1" + "</em>",
+        link: "<a href=\"" + "$2" + "\">" + "$1" + "</a>",
+        para: "<p" + " class=\"newP\">" + "   " + "$1" + "</p>"
+    };
+
+    window.parse = function parse(text) {
+        for (var key in replacer) {
+            if (replacer.hasOwnProperty(key)) {
+                text = replacer[key](text);
+            }
+        }
+        return text;
+    }
+})(window);
